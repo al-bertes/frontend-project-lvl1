fbb50691bc46caf7fc116d687caa06651276d6f9
Albet Savchenko <askoltpupkov@gmail.com>
2021-11-13 13:32:05 +1000

    step1.0

---
+++ bin/brain-games.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { getName } from "../src/cli.js";
+
+getName();
+// import readlineSync from 'readline-sync';
+// console.log('Welcome to the Brain Games!')
+//     const name = readlineSync.question('May I have your name? ')
+//     console.log(`Hello, ${name}!`);
