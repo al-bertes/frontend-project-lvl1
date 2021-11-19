9ef6176de7afa8eb136541192cdb13d243e7c747
Albet Savchenko <askoltpupkov@gmail.com>
2021-11-19 20:56:34 +1000

    try finish project2

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import { getName } from "../src/cli.js";
-import { checkByEvenNumber } from '../src/games/brain-even.js';
 
 
 console.log('Welcome to the Brain Games!');
@@ -9,6 +8,5 @@ console.log('Welcome to the Brain Games!');
 const nameUser = readlineSync.question('May I have your name? ');
 
 getName(nameUser);
-checkByEvenNumber(nameUser);
 
 
